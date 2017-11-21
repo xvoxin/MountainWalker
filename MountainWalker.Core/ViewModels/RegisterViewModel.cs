b9572d0517b798d93b379284f774c93438ba8ad1
Patryk Matuszak <matuszak.patryk@wp.pl>
2017-11-21 23:24:56 +0100

    add validation

--- MountainWalker.Core/ViewModels/RegisterViewModel.cs
+++ MountainWalker.Core/ViewModels/RegisterViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel;
 using System.Text;
 using System.Threading.Tasks;
 using MountainWalker.Core.Interfaces;
 using MvvmCross.Core.ViewModels;
 using MvvmCross.Platform;
+using System.Text.RegularExpressions;
 
 namespace MountainWalker.Core.ViewModels
 {
@@ -57,16 +59,61 @@ namespace MountainWalker.Core.ViewModels
         }
 
         public IMvxCommand RegisterButton => new MvxCommand(Walidate);
+
         private void Walidate()
         {
-            if (_registerService.CheckData(_name,_surname,_login,_password,_repPassword,_email))
+            if (CheckName(_name) && CheckName(_surname) && CheckLogin(_login) && CheckPassword(_password) && CheckPassword(_repPassword) && CheckEmail(_email) && (_password.Equals(_repPassword)))
             {
+                //TODO: Insert new user into DB
                 ShowViewModel<MainViewModel>();
             }
             else
             {
                 _dialogService.ShowAlert("Uwaga!", "Dane są nieprawidłowe", "OK");
             }
+        }
+
+        private Boolean CheckName(string name)
+        {
+            if ((name.Length <= 1) && (name.Length >= 20))
+                return false;
+            if (name.ToCharArray().Any(char.IsDigit))
+                return false;
+            if (name.ToCharArray().Any(char.IsSymbol))
+                return false;
+            return true;
+        }
+
+        private Boolean CheckLogin(string login)
+        {
+            if ((login.Length <= 2) && (login.Length >= 12))
+                return false;
+            if (login.ToCharArray().Any(char.IsSymbol))
+                return false;
+            if (Regex.IsMatch(login, @"^\d+"))
+                return false;
+
+            //TODO: Check if login is taken
+
+            return true;
+
+        }
+
+        private Boolean CheckPassword(string password)
+        {
+            if ((password.Length <= 5) && (password.Length >= 20))
+                return false;
+            return true;
+
+        }
+
+        private Boolean CheckEmail(string email)
+        {
+            if ((email.Length <= 5) && (email.Length >= 20))
+                return false;
+            if (Regex.IsMatch(email, @"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|" + @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)" + @"@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$"))
+                return false;
+            return true;
         }
     }
 }
